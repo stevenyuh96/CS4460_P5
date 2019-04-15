3aa477db18159dab978d9e1238ce0b0196f283d2
Steven Yuh <stevenyuh96@gmail.com>
2019-04-15 16:25:15 -0400

    Empty files
    added empty main.js, index.html, and styles.css

---
+++ main.js
@@ -0,0 +1,13 @@
+// Your browser will call the onload() function when the document
+// has finished loading. In this case, onload() points to the
+// start() method we defined below. Because of something called
+// function hoisting, the start() method is callable on line 6
+// even though it is defined on line 8.
+window.onload = start;
+
+// This is where all of our javascript code resides. This method
+// is called by "window" when the document (everything you see on
+// the screen) has finished loading.
+function start() {
+
+}
